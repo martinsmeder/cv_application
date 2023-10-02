f77027dfaa040b4c425fa2a6e2a45fba26ecbcb4
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-02 21:06:25 +0200

    Practiced rendering techniques and keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+const recipes = [
+  {
+    id: 'greek-salad',
+    name: 'Greek Salad',
+    ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta'],
+  },
+  {
+    id: 'hawaiian-pizza',
+    name: 'Hawaiian Pizza',
+    ingredients: [
+      'pizza crust',
+      'pizza sauce',
+      'mozzarella',
+      'ham',
+      'pineapple',
+    ],
+  },
+  {
+    id: 'hummus',
+    name: 'Hummus',
+    ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'],
+  },
+];
+
+function getRecipes() {
+  return recipes.map((recipe) => (
+    <li key={recipe.id}>{`
+    Recipe: ${recipe.name}.
+    Ingredients: ${recipe.ingredients.map((ingredient) => ` ${ingredient}`)}
+    `}</li>
+  ));
+}
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>Reacting!</h1>
-      </div>
-    </>
+    <div>
+      <h1>Recipes</h1>
+      <ul>{getRecipes()}</ul>
+    </div>
   );
 }
 
