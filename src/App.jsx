83e83e35e2d42b0a677c318cd8c211f127d5eca9
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-03 20:45:01 +0200

    Added code to experiment with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,58 @@
-const recipes = [
+import { useState } from 'react';
+
+const sculptureList = [
   {
-    id: 'greek-salad',
-    name: 'Greek Salad',
-    ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta'],
+    name: 'Homenaje a la Neurocirugía',
+    artist: 'Marta Colvin Andrade',
+    description:
+      'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+    url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+    alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.',
   },
   {
-    id: 'hawaiian-pizza',
-    name: 'Hawaiian Pizza',
-    ingredients: [
-      'pizza crust',
-      'pizza sauce',
-      'mozzarella',
-      'ham',
-      'pineapple',
-    ],
+    name: 'Floralis Genérica',
+    artist: 'Eduardo Catalano',
+    description:
+      'This enormous (75 ft. or 23m) silver flower is located in Buenos Aires. It is designed to move, closing its petals in the evening or when strong winds blow and opening them in the morning.',
+    url: 'https://i.imgur.com/ZF6s192m.jpg',
+    alt: 'A gigantic metallic flower sculpture with reflective mirror-like petals and strong stamens.',
   },
   {
-    id: 'hummus',
-    name: 'Hummus',
-    ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'],
+    name: 'Eternal Presence',
+    artist: 'John Woodrow Wilson',
+    description:
+      'Wilson was known for his preoccupation with equality, social justice, as well as the essential and spiritual qualities of humankind. This massive (7ft. or 2,13m) bronze represents what he described as "a symbolic Black presence infused with a sense of universal humanity."',
+    url: 'https://i.imgur.com/aTtVpES.jpg',
+    alt: 'The sculpture depicting a human head seems ever-present and solemn. It radiates calm and serenity.',
   },
 ];
 
-function getRecipes() {
-  return recipes.map((recipe) => (
-    <li key={recipe.id}>{`
-    Recipe: ${recipe.name}.
-    Ingredients: ${recipe.ingredients.map((ingredient) => ` ${ingredient}`)}
-    `}</li>
-  ));
-}
+export default function App() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
-function App() {
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div>
-      <h1>Recipes</h1>
-      <ul>{getRecipes()}</ul>
-    </div>
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <button onClick={handleMoreClick}>{showMore ? 'Hide' : 'Show'}</button>
+      {showMore && <p>{sculpture.description}</p>}
+    </>
   );
 }
-
-export default App;
