dbbf3d91c6738052a03f31a4ccf9be6d59e6a48d
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-06 19:14:47 +0200

    Added some exercises related to structuring state

--- src/Greeting.jsx
+++
@@ -1,5 +0,0 @@
-function Greeting() {
-  return <h1>&quot;What am I doing?&quot;</h1>;
-}
-
-export default Greeting;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Greeting from './Greeting.jsx';
 import Robot from './Robot.jsx';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Greeting />
     <Robot />
     <App />
   </React.StrictMode>
