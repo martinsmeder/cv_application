ee6b31b1662280c895fb1d4ffeca033f1071f4e1
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-11 18:53:48 +0200

    Added Education components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,98 @@
 import { useState } from 'react';
 
-function Forms({ fullNameChange, emailChange, phoneChange, addressChange }) {
+function ContactForm({
+  fullNameChange,
+  emailChange,
+  phoneChange,
+  addressChange,
+}) {
   return (
-    <div id='formContainer'>
-      <section className='formSection contacts'>
-        <form className='form'>
-          <input
-            id='fullName'
-            type='text'
-            placeholder='Full name:'
-            onChange={fullNameChange}
-          />
-          <input
-            id='email'
-            type='text'
-            placeholder='Email:'
-            onChange={emailChange}
-          />
-          <input
-            id='phone'
-            type='text'
-            placeholder='Phone:'
-            onChange={phoneChange}
-          />
-          <input
-            id='adress'
-            type='text'
-            placeholder='Adress:'
-            onChange={addressChange}
-          />
-        </form>
-      </section>
+    <form className='form'>
+      <input
+        id='fullName'
+        type='text'
+        placeholder='Full name:'
+        onChange={fullNameChange}
+      />
+      <input
+        id='email'
+        type='text'
+        placeholder='Email:'
+        onChange={emailChange}
+      />
+      <input
+        id='phone'
+        type='text'
+        placeholder='Phone:'
+        onChange={phoneChange}
+      />
+      <input
+        id='adress'
+        type='text'
+        placeholder='Adress:'
+        onChange={addressChange}
+      />
+    </form>
+  );
+}
+
+function EducationForm({ schoolChange, titleChange, dateChange }) {
+  return (
+    <form className='form'>
+      <input
+        id='schoolName'
+        type='text'
+        placeholder='School name:'
+        onChange={schoolChange}
+      />
+      <input
+        id='studyTitle'
+        type='text'
+        placeholder='Title of study:'
+        onChange={titleChange}
+      />
+      <input
+        id='studyDates'
+        type='text'
+        placeholder='Date of study:'
+        onChange={dateChange}
+      />
+    </form>
+  );
+}
+
+function Cv({ contacts, education }) {
+  return (
+    <div id='cvContainer'>
+      <CvContacts contacts={contacts} />
+      <CvEducation education={education} />
     </div>
   );
 }
 
-function Cv({ contacts }) {
+function CvContacts({ contacts }) {
   return (
-    <div id='cvContainer'>
-      <section className='cvSection contacts'>
-        <h1>{contacts.fullName}</h1>
-        <div>
-          <p>{contacts.email}</p>
-          <p>{contacts.phone}</p>
-          <p>{contacts.address}</p>
-        </div>
-      </section>
-    </div>
+    <section className='cvSection contacts'>
+      <h1>{contacts.fullName}</h1>
+      <div>
+        <p>{contacts.email}</p>
+        <p>{contacts.phone}</p>
+        <p>{contacts.address}</p>
+      </div>
+    </section>
+  );
+}
+
+function CvEducation({ education }) {
+  return (
+    <section className='cvSection education'>
+      <h1>EDUCATION</h1>
+      <div>
+        <p>{education.schoolName}</p>
+        <p>{education.studyTitle}</p>
+        <p>{education.studyDates}</p>
+      </div>
+    </section>
   );
 }
 
@@ -58,47 +104,53 @@ export default function App() {
     address: '?',
   });
 
-  function handleFullNameChange(e) {
+  const [education, setEducation] = useState({
+    schoolName: '?',
+    studyTitle: '?',
+    studyDates: '?',
+  });
+
+  function handleContactChange(e, propertyName) {
     setContactDetails({
-      ...contactDetails, // Copy old fields
-      fullName: e.target.value, // Override only this field
+      ...contactDetails, // Create a copy of the original object
+      [propertyName]: e.target.value, // Update this specific property only
     });
   }
 
-  function handleEmailChange(e) {
-    setContactDetails({
-      ...contactDetails,
-      email: e.target.value,
-    });
-  }
-
-  function handlePhoneChange(e) {
-    setContactDetails({
-      ...contactDetails,
-      phone: e.target.value,
-    });
-  }
-
-  function handleAddressChange(e) {
-    setContactDetails({
-      ...contactDetails,
-      address: e.target.value,
+  function handleEducationChange(e, propertyName) {
+    setEducation({
+      ...education, // Create a copy of the original object
+      [propertyName]: e.target.value, // Update this specific property only
     });
   }
 
   return (
     <main>
-      <Forms
-        fullNameChange={(e) => handleFullNameChange(e)}
-        emailChange={(e) => handleEmailChange(e)}
-        phoneChange={(e) => handlePhoneChange(e)}
-        addressChange={(e) => handleAddressChange(e)}
-      />
-      <Cv contacts={contactDetails} />
+      <div id='formContainer'>
+        <section className='formSection contacts'>
+          <ContactForm
+            fullNameChange={(e) => handleContactChange(e, 'fullName')}
+            emailChange={(e) => handleContactChange(e, 'email')}
+            phoneChange={(e) => handleContactChange(e, 'phone')}
+            addressChange={(e) => handleContactChange(e, 'address')}
+          />
+        </section>
+        <section className='formSection education'>
+          <EducationForm
+            schoolChange={(e) => handleEducationChange(e, 'schoolName')}
+            titleChange={(e) => handleEducationChange(e, 'studyTitle')}
+            dateChange={(e) => handleEducationChange(e, 'studyDates')}
+          />
+          <button>+</button>
+        </section>
+      </div>
+      <Cv contacts={contactDetails} education={education} />
     </main>
   );
 }
 
-// Get changes in form to be displayed in cv immediately
-// --> Move sections into their own components, get that to work as well
-// --> Move on
+// Initial array with one object with question marks
+// CvEducation just renders what's in the array currently
+// + button opens form
+// save button adds to array, and displays array to the left
+// edit button repopens form for that particular object, with prev filled values
