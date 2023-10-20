82f1b86d5e8def4f7981a140b899aa00a1e45920
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-20 18:24:19 +0200

    Added media queries

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
-// import Test from './components/Test';
 import './styles/main.css';
 import './styles/cv.css';
 import './styles/forms.css';
@@ -9,6 +8,5 @@ import './styles/forms.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* <Test /> */}
   </React.StrictMode>
 );
